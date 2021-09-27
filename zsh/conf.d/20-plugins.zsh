c8172c1d18f3e5e8599dd1b18bdcbaf15976acf2
Daniel Bowring <git@daniel.bowring.email>
2021-09-27 15:26:43 +0900

    Remove django plugin

--- zsh/conf.d/20-plugins.zsh
+++ zsh/conf.d/20-plugins.zsh
@@ -5,11 +5,11 @@ if ! zgen saved; then
   zgen oh-my-zsh plugins/archlinux
   zgen oh-my-zsh plugins/cargo
   zgen oh-my-zsh plugins/command-not-found
-  zgen oh-my-zsh plugins/django
   zgen oh-my-zsh plugins/docker
   zgen oh-my-zsh plugins/docker-compose
   zgen oh-my-zsh plugins/history-substring-search
   zgen oh-my-zsh plugins/npm
+  zgen oh-my-zsh plugins/nvm
   zgen oh-my-zsh plugins/pip
   zgen oh-my-zsh plugins/rust
   zgen oh-my-zsh plugins/rustup
@@ -19,7 +19,6 @@ if ! zgen saved; then
   zgen load zsh-users/zsh-completions src
   # zgen load zsh-users/zsh-history-substring-search
   # zgen load zsh-users/zsh-syntax-highlighting
-  
   zgen load caiogondim/bullet-train-oh-my-zsh-theme bullet-train
 
   zgen save
