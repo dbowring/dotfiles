fe38b0de3e1461975aad9fc475b563928c20e357
Daniel Bowring <git@daniel.bowring.email>
2021-10-24 01:42:24 +0900

    Add cursor movement binds

--- zsh/conf.d/50-keys.zsh
+++ zsh/conf.d/50-keys.zsh
@@ -5,6 +5,20 @@
 bindkey '^[[1;3D' backward-word
 bindkey '^[[1;3C' forward-word
 
+# Home/end
+bindkey  "^[[1~"   beginning-of-line
+bindkey  "^[[4~"   end-of-line
+
+# Custom word deletion
+custom-backward-delete-word () {
+   local WORDCHARS='!#$%&()*;<=>?[]^{}~'
+   zle backward-delete-word
+}
+zle -N custom-backward-delete-word
+# (ctrl|alt)+delete
+bindkey '^[^?' custom-backward-delete-word
+bindkey '^H' custom-backward-delete-word
+bindkey '\e^?' custom-backward-delete-word
 
 # Configure plugin history-substring-search
 bindkey '^[[A' history-substring-search-up
