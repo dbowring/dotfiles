02737f3928147882d13f2bd2ee73ae9d677d7a98
Daniel Bowring <git@daniel.bowring.email>
2021-10-25 10:07:37 +0900

    Fix mac support

--- zsh/conf.d/20-plugins.zsh
+++ zsh/conf.d/20-plugins.zsh
@@ -1,13 +1,26 @@
 
-plugins=(
-    /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-    /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
-    /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-);
+local _plugins=()
 
-for plugin in $plugins; do
+if command -v brew > /dev/null; then
+    # Mac w/homebrew
+    local _prefix=$(brew --prefix)
+    fpath+=( $_prefix/share/zsh-completions )
+    _plugins+=(
+        $_prefix/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+        $_prefix/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+        $_prefix/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    )
+else
+    # Linux
+    _plugins+=(
+        /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+        /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+        /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+    );
+    # zsh-completions is typically already on fpath for system package
+    # installation, so don't need to updadte fpath
+fi
+
+for plugin in $_plugins; do
     source "$plugin"
 done
-
-# zsh-completions is also recommended (typically automatically added to fpath
-# by package manager)
