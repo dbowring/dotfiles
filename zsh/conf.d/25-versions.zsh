059a0173a41bee3d8055c218d66ebb690f77ebb0
Daniel Bowring <daniel@spaceduck.com>
2024-03-14 14:38:57 +1100

    Setup mise

--- zsh/conf.d/25-versions.zsh
+++ zsh/conf.d/25-versions.zsh
@@ -4,3 +4,7 @@ if (command -v pyenv > /dev/null); then
     [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
 fi
+
+if (command -v mise > /dev/null); then
+    eval "$(mise activate zsh)"
+fi
