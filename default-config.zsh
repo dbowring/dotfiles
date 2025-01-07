bbedec6c882e9d182747725479a1b41de4f995c0
Daniel Bowring <daniel@spaceduck.com>
2025-01-07 18:01:08 +1100

    Migrate to bare repo
    https://www.atlassian.com/git/tutorials/dotfiles

--- default-config.zsh
+++
@@ -1,8 +0,0 @@
-# Path to zsh binary
-export ZSH_FULL_NAME=$(which $ZSH_NAME)
-# Path to tmux binary
-export TMUX_FULL_NAME=$(which tmux)
-
-# Alacritty options
-export ALACRITTY_FONT_SIZE=11
-export TMUX_SESSION_NAME=alacritty
