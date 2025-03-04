da43186dd54928c2c10f2607929d6ff91406bce5
Daniel Bowring <daniel@spaceduck.com>
2025-03-05 01:14:14 +1100

    Add alias for standard helix binary name

--- .config/zsh/conf.d/60-aliases.zsh
+++ .config/zsh/conf.d/60-aliases.zsh
@@ -11,3 +11,7 @@ fi
 if (command -v vopono > /dev/null); then
     alias withvpn='vopono exec --provider mullvad --server ${MULLVAD_SERVER_REGION:-usa}'
 fi
+
+if (command -v helix > /dev/null); then
+    alias hx=helix
+fi
