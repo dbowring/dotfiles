7abf52ba7d692a207cf10a9fe428d90ce752f520
Daniel Bowring <daniel@spaceduck.com>
2025-06-25 11:30:12 +1000

    Add configuration for zoxide

--- .config/zsh/conf.d/25-extensions.zsh
+++ .config/zsh/conf.d/25-extensions.zsh
@@ -1,4 +1,12 @@
 
+if (command -v starship > /dev/null); then
+  eval "$(starship init zsh)"
+fi
+
+if (command -v zoxide > /dev/null); then
+  eval "$(zoxide init zsh)"
+fi
+
 if (command -v pyenv > /dev/null); then
     export PYENV_ROOT="$HOME/.pyenv"
     [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
