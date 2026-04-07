5443a82f536af68022951ddea1a7613c6e31307b
Daniel Bowring <git@daniel.bowring.email>
2026-04-07 11:56:27 +1000

    Fix mac support for zsh

--- .config/zsh/conf.d/20-plugins.zsh
+++ .config/zsh/conf.d/20-plugins.zsh
@@ -1,10 +1,26 @@
 local _plugins=()
 
-if command -v brew > /dev/null; then
+if [[ -x /opt/homebrew/bin/brew ]]; then
     # Mac w/homebrew
     local _prefix=$(brew --prefix)
     fpath+=( $_prefix/share/zsh-completions )
     _plugins+=(
+        $_prefix/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+        $_prefix/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+        $_prefix/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    )
+elif [[ -x /opt/pkg/bin/pkgin ]]; then
+    # Mac w/pkgin
+    local _prefix=/opt/pkg/
+   _plugins+=(
+        $_prefix/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+        $_prefix/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+        $_prefix/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+    )
+elif [[ -x /opt/local/bin/port ]]; then
+    # Mac w/macports
+    local _prefix=/opt/local
+   _plugins+=(
         $_prefix/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
         $_prefix/share/zsh-history-substring-search/zsh-history-substring-search.zsh
         $_prefix/share/zsh-autosuggestions/zsh-autosuggestions.zsh
